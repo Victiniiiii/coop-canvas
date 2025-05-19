ae4833e2e0930db9276826740d6de5c7eec3e9a4
Victiniiiii <victinii494@gmail.com>
2025-05-19 18:48:24 +0300

    really hope nobody is seeing these commits....

--- app/board/[id]/page.tsx
+++ app/board/[id]/page.tsx
@@ -1,16 +1,16 @@
-import { GetServerSidePropsContext } from "next";
 import { prisma } from "@/lib/db";
 import BoardClient from "./client";
 
-export default async function BoardPage({ params }: { params: { id: string } }) {
+export default async function BoardPage({ params }) {
+    const { id } = params;
     const board = await prisma.board.findUnique({
-        where: { id: params.id },
+        where: { id },
     });
 
     return (
         <main>
-            <h1>Board {params.id}</h1>
-            <BoardClient boardId={params.id} />
+            <h1>Board {id}</h1>
+            <BoardClient boardId={id} />
         </main>
     );
 }
