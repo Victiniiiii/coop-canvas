0fb27e8dd8f82274c8890076c388adede7258772
Victiniiiii <victinii494@gmail.com>
2025-05-21 12:25:35 +0300

    very small main menu changes

--- app/page.tsx
+++ app/page.tsx
@@ -15,8 +15,8 @@ export default function Home() {
 		<main className="flex flex-col justify-around items-center text-white">
 			<h1 className="text-center text-7xl mt-2 text-shadow font-extrabold">Collaborative Canvas</h1>
 			<p className="text-2xl text-shadow">This project is a real-time collaborative drawing board built with Next.js, TypeScript, Pusher, PostgreSQL and Supabase.</p>
-			<p className="text-2xl text-shadow">No authentication required — just create or join a board by URL.</p>
-			<button className="bg-blue-500 p-4 rounded-lg border-white border" onClick={createNewBoard}>Create New Board</button>
+			<p className="text-2xl text-shadow">No authentication required, just create or join a board by URL.</p>
+			<button className="bg-blue-500 p-4 rounded-lg cursor-pointer border-white border" onClick={createNewBoard}>Create New Board</button>
 		</main>
 	);
 }
