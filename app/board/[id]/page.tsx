7961ac884b9dc8397e7e25c75a98b69cd5b8bff3
Victiniiiii <victinii494@gmail.com>
2025-05-21 22:00:49 +0300

    better styling in the drawing page

--- app/board/[id]/page.tsx
+++ app/board/[id]/page.tsx
@@ -8,8 +8,8 @@ export default async function BoardPage({ params }: { params: Promise<{ id: stri
 	});
 
 	return (
-		<main className="flex flex-col items-center min-h-screen w-full px-2 py-4">
-			<h1 className="w-full md:max-w-screen-xl mx-auto h-10 flex items-center justify-center font-bold bg-white bg-opacity-50 rounded-md mb-4 text-black">Board {id}</h1>
+		<main className="flex flex-col items-center min-h-screen w-full">
+			<h1 className="w-full mx-auto h-10 flex items-center justify-center font-bold bg-white bg-opacity-50 rounded-md mb-4 text-black">Board {id}</h1>
 			<BoardClient boardId={id} />
 		</main>
 	);
