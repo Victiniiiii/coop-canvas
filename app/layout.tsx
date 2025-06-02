6a042d943fbaf003eafecc59dd2c8156afcdd2a3
Victiniiiii <victinii494@gmail.com>
2025-06-02 13:33:04 +0300

    added favicon to all pages

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,6 +11,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 		<html lang="en">
 			<head>
 				<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+				<link rel="icon" href="/vic32.png" type="image/png" />
 			</head>
 			<body className="bg-gradient-to-br from-blue-900 to-purple-800 text-white">
 				{children}
